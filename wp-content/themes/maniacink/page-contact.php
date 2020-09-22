44e2e69617c9cc43c087f0b7ca3e0123144407bf
Helvin Rymer <helvinrymer@Helvins-Air.home>
2020-09-22 17:46:57 -0400

    updates

--- wp-content/themes/maniacink/page-contact.php
+++ wp-content/themes/maniacink/page-contact.php
@@ -21,9 +21,9 @@
 
 
 
-    <div class="container p-3">
+    <div class="container p-3 text-center">
       <h1 class="sedgewick display-1">Contact</h1>
-      <div class="container">
+      <div class="container text-center">
         <!-- <form action="<?php //get_theme_file_uri('dhcontact.php')?>" method="post">
           <label for="name" class="p-2">Full Name: <br>
             <input type="text" name="name">

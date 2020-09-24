5ec2e48cfaffbda825fe09ab4e847b8e5e2d192d
Helvin Rymer <helvinrymer@Helvins-Air.home>
2020-09-23 23:09:21 -0400

    updates

--- wp-content/themes/maniacink/js/modules/newMessage.js
+++ wp-content/themes/maniacink/js/modules/newMessage.js
@@ -12,25 +12,25 @@ class newMessage {
 
   // methods
   messageDispatcher() {
-    var newMessage = {
-      name: $("#name").val(),
-      phone: $("#pNumber").val(),
-      email: $("#email"),
-      explanation: $("#explanation").val(),
-      concept: $("#concept").val(),
-      covid: $("#covid").val(),
-    };
-
-    $.ajax({
-      beforeSend: (xhr) => {
-        xhr.setRequestHeader("XP-WP-Nonce", maniacData);
-      },
-      url: maniacData.root_url + "wp-json/wp/v2/contact/",
-      type: "POST",
-      data: newMessage,
-      success: maniacData.root_url + "wp-json/wp/v2/contact/3",
-      error: x,
-    });
+    // var newMessage = {
+    //   name: $("#name").val(),
+    //   phone: $("#pNumber").val(),
+    //   email: $("#email"),
+    //   explanation: $("#explanation").val(),
+    //   concept: $("#concept").val(),
+    //   covid: $("#covid").val(),
+    // };
+    // $.ajax({
+    //   beforeSend: (xhr) => {
+    //     xhr.setRequestHeader("XP-WP-Nonce", maniacData);
+    //   },
+    //   url: maniacData.root_url + "wp-json/wp/v2/contact/",
+    //   type: "POST",
+    //   data: newMessage,
+    //   success: maniacData.root_url + "wp-json/wp/v2/contact/",
+    //   error: x,
+    // });
+    alert("whatsup");
   }
 }
 

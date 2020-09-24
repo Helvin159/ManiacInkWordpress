4fe232d4fa6c5d308fe446c1386fdb78b1816b77
Helvin Rymer <helvinrymer@Helvins-Air.home>
2020-09-23 23:18:39 -0400

    updates

--- wp-content/themes/maniacink/header.php
+++ wp-content/themes/maniacink/header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php wp_head(); ?>
-		<link rel="stylesheet" href="<?php echo get_theme_file_uri('css/style.css')?>">
+		<!-- <link rel="stylesheet" href="<?php //echo get_theme_file_uri('css/style.css')?>"> -->
 </head>
 <body>
 
